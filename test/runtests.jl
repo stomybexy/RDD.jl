956b4a90f6484ef43479c2ff1c8d8428afd20f4c
Jonatan Sombié <jonatan.sombie@gmail.com>
2019-05-18 19:20:05 +0200

    add default implem for rdd interface

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,19 +3,25 @@ using Test
 using RDD
 
 @testset "define AbstractRDD interface" begin
-    @testset "partitions function is defined" begin
-        @test length(methods(partitions).ms) ≥ 0
+
+    struct TestRDD{T} <: AbstractRDD{T} end
+    struct TestPartition{T} <: AbstractPartition{T} end
+    rdd = TestRDD{Int}()
+    part = TestPartition{Int}()
+
+    @testset "partitions interface must be defined" begin
+        @test_throws ErrorException partitions(rdd)
     end
     @testset "preferredlocations function is defined" begin
-        @test length(methods(preferredlocations).ms) ≥ 0
+        @test_throws ErrorException preferredlocations(rdd, part)
     end
     @testset "dependencies function is defined" begin
-        @test length(methods(dependencies).ms) ≥ 0
+        @test_throws ErrorException dependencies(rdd)   
     end
     @testset "iterator function is defined" begin
-        @test length(methods(iterator).ms) ≥ 0
+        @test_throws ErrorException iterator(rdd, part, AbstractPartitionIterator[])
     end
     @testset "partitioner function is defined" begin
-        @test length(methods(partitioner).ms) ≥ 0
+        @test_throws ErrorException partitioner(rdd)
     end
 end
