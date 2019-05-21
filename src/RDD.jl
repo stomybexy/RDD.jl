1ef63c64fd77e9dd8a54ebecc79eb63115c98d9e
Jonatan Sombié <jonatan.sombie@gmail.com>
2019-05-21 20:26:35 +0200

    add a basic ParallelCollectionRDD implementation

--- src/RDD.jl
+++ src/RDD.jl
@@ -1,16 +1,20 @@
 module RDD
 
+using Distributed, Random
+
 export AbstractRDD,
     AbstractPartition,
     AbstractDependency,
     AbstractPartitioner,
+    AbstractPartitionIterator,
     partitions,
     preferredlocations,
     dependencies,
     iterator,
     partitioner,
     ParallelCollectionRDD,
-    ParallelCollectionPartition
+    ParallelCollectionPartition,
+    ParallelCollectionPartitionIterator
 
 include("./AbstractRDD.jl")
 include("./ParallelCollectionRDD.jl")
