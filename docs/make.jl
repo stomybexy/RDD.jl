a6b684cdbbc99ceea7d43b3974f8a50c643a23e8
Jonatan Sombié <jonatan.sombie@gmail.com>
2019-06-09 16:20:54 +0200

    add travis documentation build

--- docs/make.jl
+++ docs/make.jl
@@ -1,6 +1,17 @@
 using Pkg
-pkg"activate .."
-push!(LOAD_PATH, "../src/")
+Pkg.activate("$(@__DIR__)/..")
+push!(LOAD_PATH, "$(@__DIR__)/../src/")
 using Documenter, RDD
 
 makedocs(sitename = "RDD Documentation")
+
+# Deploy built documentation from Travis.
+# =======================================
+
+deploydocs(
+    # options
+    repo = "github.com/stomybexy/RDD.jl.git",
+    target = "build",
+    deps = nothing,
+    make = nothing
+)
