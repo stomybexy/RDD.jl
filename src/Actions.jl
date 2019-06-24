2de3b348c4995b1332709f3d066eea534a28814c
Jonatan Sombié <jonatan.sombie@gmail.com>
2019-06-24 20:06:13 +0200

    use Dagger jl as DAG scheduler

--- src/Actions.jl
+++ src/Actions.jl
@@ -1,6 +1,8 @@
 module Actions
 
-using Distributed, ..AbstractRDDModule
+using Distributed, Dagger
+
+using ..AbstractRDDModule
 
 import Base: reduce
 
@@ -11,9 +13,10 @@ Reduce the elements of `rdd` using the binary function `f` with the init value `
 `f`must be commutative and associative and `init` must be neutral for `f`. 
 """
 function reduce(f, rdd::AbstractRDD{T}, init::T) where T
-    @distributed f for partition in 1:1:partitions(rdd)
-        reduce(f, (x for x in compute(rdd, partition)), init)
-    end
+    reduce_partition(rdd, partition) = reduce(f, (x for x in AbstractRDDModule.compute(rdd, partition)), init)
+    partitions_results = (delayed(reduce_partition)(rdd, partition) for partition in 1:1:partitions(rdd))
+
+    delayed(reduce)(f, init, partitions_results...) |> collect
 end
 
 function reduce(f, gen::Base.Generator, init) 
@@ -23,5 +26,7 @@ function reduce(f, gen::Base.Generator, init)
     end
     r
 end
+
+reduce(f, init, values...) = reduce(f, values, init=init)
 
 end
